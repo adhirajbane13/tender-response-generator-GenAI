48a6c1ddfea0beb1884dd7f434440bcb108b84dd
adhirajbane13 <adhirajbane13@gmail.com>
2025-04-11 11:25:51 +0100

    font file loc updated

--- src/pdf_exporter.py
+++ src/pdf_exporter.py
@@ -3,7 +3,7 @@ from fpdf import FPDF
 def export_response_to_pdf(question: str, answer: str, context_chunks: list[str], filename: str = "response.pdf"):
     pdf = FPDF()
     pdf.add_page()
-    pdf.add_font('Arial', '', '..\\fonts\\arial.ttf', uni=True)
+    pdf.add_font('Arial', '', '../fonts/arial.ttf', uni=True)
     pdf.set_font("Arial", size=12)
 
     pdf.multi_cell(0, 10, f"Question:\n{question}\n\n", align='L')
